9a642f1d8b892dd7017fb28f7e4392a2d3e350a2
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-09-09 06:10:49 -0400

    Edit .detignore so that the successful output of context_files.py is empty

--- detignore_wildcard/context_files.py
+++ detignore_wildcard/context_files.py
@@ -2,6 +2,8 @@ import determined.common.context
 import pathlib
 blah = determined.common.context.read_v1_context(pathlib.Path("."))
 print()
-for _file in sorted([_b.path for _b in blah]):
-    print(_file)
-
+if blah:
+    for _file in sorted([_b.path for _b in blah]):
+        print(_file)
+else:
+    print("No files in context. Is the bug fixed??")
